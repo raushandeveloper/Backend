d05281631992fe7c170037f426cfa01922420f9a
RaushanDevloper <raushankumarsingh7033@gmail.com>
2026-07-26 18:27:07 +0530

    fixed nested repo issue

--- Database/Chapter_17-Introduction_Mongoose/models/favourite.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const favouriteSchema = mongoose.Schema({
-  houseId: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Favourite', favouriteSchema);
